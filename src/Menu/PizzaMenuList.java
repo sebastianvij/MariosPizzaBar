758ae827ed1be74f4fb5564eea6f84e05628b9c0
Gotze <thorholby@gmail.com>
2025-03-26 21:39:21 +0100

    Ændret "magic number" 30 til variabel default large size price increase

--- src/Menu/PizzaMenuList.java
+++ src/Menu/PizzaMenuList.java
@@ -7,6 +7,8 @@ import java.util.Arrays;
 
 public class PizzaMenuList {
 
+    private static final int DEFAULT_LARGE_SIZE_PRICE_INCREASE = 30;
+
     public static final ArrayList<Pizza> pizzaList = new ArrayList<>();
 
     static {
@@ -22,7 +24,7 @@ public class PizzaMenuList {
         pizzaList.add(new Pizza("Vegetariana", 68, 10, new String[]{"Tomatsauce", "Ost", "Champignon", "Løg", "Paprika", "Oliven", "Artiskok"}));
         pizzaList.add(new Pizza("Diavola", 70, 11, new String[]{"Tomatsauce", "Ost", "Pepperoni", "Chili"}));
         pizzaList.add(new Pizza("Calzone", 74, 12, new String[]{"Tomatsauce", "Ost", "Skinke", "Champignon"}));
-        pizzaList.add(new Pizza("Glizzy Pizza", 60, 13, new String[]{"Tomatsauce", "Ost", "Cocktailpølser"}));
+        pizzaList.add(new Pizza("Glizzy", 60, 13, new String[]{"Tomatsauce", "Ost", "Cocktailpølser"}));
         pizzaList.add(new Pizza("Prosciutto", 73, 14, new String[]{"Tomatsauce", "Ost", "Parmaskinke", "Rucola"}));
         pizzaList.add(new Pizza("Tonno", 71, 15, new String[]{"Tomatsauce", "Ost", "Tun", "Løg"}));
         pizzaList.add(new Pizza("Salami", 69, 16, new String[]{"Tomatsauce", "Ost", "Stærk Salami"}));
@@ -31,15 +33,15 @@ public class PizzaMenuList {
         pizzaList.add(new Pizza("Gorgonzola", 67, 19, new String[]{"Tomatsauce", "Ost", "Gorgonzola", "Løg"}));
         pizzaList.add(new Pizza("Æg og Bacon", 70, 20, new String[]{"Tomatsauce", "Ost", "Bacon", "Æg", "Løg"}));
         pizzaList.add(new Pizza("Fiskefilet", 80, 21, new String[]{"Tomatsauce", "Ost", "Paneret Fiskefilet", "Remoulade"}));
-        pizzaList.add(new Pizza("Pommes Frites Pizza", 79, 22, new String[]{"Tomatsauce", "Ost", "Pommes Frites", "Kebabkød", "Dressing"}));
+        pizzaList.add(new Pizza("Pommes Frites", 79, 22, new String[]{"Tomatsauce", "Ost", "Pommes Frites", "Kebabkød", "Dressing"}));
         pizzaList.add(new Pizza("Mexicana", 85, 23, new String[]{"Tomatsauce", "Ost", "Nacho Chips", "Kødstrimler", "Jalapeños", "Guacamole"}));
         pizzaList.add(new Pizza("Burger Pizza", 82, 24, new String[]{"Tomatsauce", "Ost", "Hakkebøffer", "Løg", "Pickles", "Burgerdressing"}));
         pizzaList.add(new Pizza("Kylling i Karry", 77, 25, new String[]{"Tomatsauce", "Ost", "Kylling", "Karry", "Ananas"}));
         pizzaList.add(new Pizza("Ansjoser og Banan", 69, 26, new String[]{"Tomatsauce", "Ost", "Ansjoser", "Banan", "Løg"}));
-        pizzaList.add(new Pizza("Chokolade Pizza", 65, 27, new String[]{"Nutella", "Marshmallows", "Banan", "Jordbær"}));
-        pizzaList.add(new Pizza("Rugbrøds Pizza", 58, 28, new String[]{"Rugbrødsbunde", "Remoulade", "Rødbeder", "Løg", "Spegepølse"}));
-        pizzaList.add(new Pizza("Sushi Pizza", 90, 29, new String[]{"Risbund", "Laks", "Avocado", "Agurk", "Soyasauce", "Wasabi"}));
-        pizzaList.add(new Pizza("Dessert Pizza", 72, 30, new String[]{"Vaniljesauce", "Marcipan", "Jordbær", "Hindbær", "Flormelis"}));
+        pizzaList.add(new Pizza("Chokolade", 65, 27, new String[]{"Nutella", "Marshmallows", "Banan", "Jordbær"}));
+        pizzaList.add(new Pizza("Rugbrød", 58, 28, new String[]{"Rugbrødsbunde", "Remoulade", "Rødbeder", "Løg", "Spegepølse"}));
+        pizzaList.add(new Pizza("Sushi", 90, 29, new String[]{"Risbund", "Laks", "Avocado", "Agurk", "Soyasauce", "Wasabi"}));
+        pizzaList.add(new Pizza("Dessert", 72, 30, new String[]{"Vaniljesauce", "Marcipan", "Jordbær", "Hindbær", "Flormelis"}));
     }
 
     public static void showPizzaMenuList() {
@@ -47,7 +49,7 @@ public class PizzaMenuList {
         System.out.printf("%-4s %-20s %-73s %6s %5s \n", "#", "Navn", "Ingredienser", "Medium", "Stor");
         System.out.println("--------------------------------------------------------------------------------------------------");
         for (Pizza pizza : pizzaList) {
-            System.out.printf("%-4d %-20s %-73s %6d,- %5d,- \n", pizza.getPizzaNum(), pizza.getName(), Arrays.toString(pizza.getPizzaToppings()), pizza.getUnitPrice(), (pizza.getUnitPrice() + 30));
+            System.out.printf("%-4d %-20s %-73s %6d,- %5d,- \n", pizza.getPizzaNum(), pizza.getName(), Arrays.toString(pizza.getPizzaToppings()), pizza.getUnitPrice(), (pizza.getUnitPrice() + DEFAULT_LARGE_SIZE_PRICE_INCREASE));
         }
         System.out.println("-*- Menukort -*-");
     }
